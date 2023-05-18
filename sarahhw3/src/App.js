e6b4ba444d0c6806958df04c3f7410a1d75cd8ce
sarahchick1971 <59619473+sarahchick1971@users.noreply.github.com>
2023-05-18 18:00:06 +0100

    code tidied

--- sarahhw3/src/App.js
+++ sarahhw3/src/App.js
@@ -6,12 +6,12 @@ import "./App.css";
 import Redirect from 'react';
 
 export const App=(props)=> {
-  // React States
+ 
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
   
 
-  // User Login info
+  
   const database = [
     {
       username: "user1",
@@ -29,18 +29,17 @@ export const App=(props)=> {
   };
 
   const handleSubmit = (event) => {
-    //Prevent page reload
+   
     event.preventDefault();
 
     var { uname, pass } = document.forms[0];
 
-    // Find user login info
     const userData = database.find((user) => user.username === uname.value);
 
-    // Compare user info
+   
     if (userData) {
       if (userData.password !== pass.value) {
-        // Invalid password
+        
         setErrorMessages({ name: "pass", message: errors.pass });
         
         
@@ -48,18 +47,18 @@ export const App=(props)=> {
         setIsSubmitted(true);
       }
     } else {
-      // Username not found
+      
       setErrorMessages({ name: "uname", message: errors.uname });
     }
   };
 
-  // Generate JSX code for error message
+  
   const renderErrorMessage = (name) =>
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
     );
 
-  // JSX code for login form
+  
   const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
@@ -80,6 +79,9 @@ export const App=(props)=> {
       </form>
     </div>
   );
+
+
+
 
   return (
     <div className="app">
